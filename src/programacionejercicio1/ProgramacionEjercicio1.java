d4bf541107df78172b4fc3ed607e91e3cddd7a1f
otorradomiguez <otorradomiguez@victoria1.danielcastelao.org>
2016-09-29 12:11:19 +0200

    project start

--- src/programacionejercicio1/ProgramacionEjercicio1.java
+++ src/programacionejercicio1/ProgramacionEjercicio1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package programacionejercicio1;
-
+import java.util.Scanner;
 /**
  *
  * @author otorradomiguez
@@ -15,9 +15,15 @@ public class ProgramacionEjercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int say;
-        say=3;
-        System.out.println(say+"+"+say);
+        
+        Scanner input= new Scanner(System.in);
+        System.out.println("Calculadora de area de rectangulos");
+        int base,altura;
+        System.out.println("Introduzca la base del rectangulo"); 
+        base = input.nextInt();
+        System.out.println("Introduzca la altura del rectangulo");
+        altura= input.nextInt();
+        System.out.println("El area es igual a "+base*altura);
     }
-    
+        
 }
